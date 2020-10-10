7f003134580e3bda9e3f0efa7d2d7a7e5f6e2a96
Anurag Ranjan <anuragranjan330@gmail.com>
2020-10-10 08:36:13 +0530

    1st exercise

--- RforDataScrience.R
+++ RforDataScrience.R
@@ -9,3 +9,20 @@ mynumaricvariable2
 mycharvariable<-c("suman","Kumar","male")
 mycharvariable
 print(sum(mynumaricvariable))
+
+##loggical vector
+mylogicalvectir<-c("TRUE","FALSE","TRUE","FALSE")
+mylogicalvector2<-c("F","T","F","F")
+
+##list
+lIst_data<-list("Suman","Kumar",c(200,40.6,50),c(TRUE,FALSE))
+lIst_data
+##factor
+derivatives_name<-c(rep("Apple",25),rep("india",20))
+derivatives_name
+derivatives_name_c<-as.factor(derivatives_name)
+derivatives_name_c
+sports<-c(rep("Crieket",10),rep("football",5),rep("tennis",10))
+sports
+sports_c<-as.factor(sports)
+sports_c
