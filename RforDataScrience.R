9502a4bb2a5c8778789278adbd7e60f7773fcba1
Anurag Ranjan <anuragranjan330@gmail.com>
2020-10-10 08:09:41 +0530

    1st commit

---
+++ RforDataScrience.R
@@ -0,0 +1,11 @@
+##data structure
+##Numeric vector
+mynumaricvariable<-c(1,2,3,4,5,5.4,-2)
+mynumaricvariable
+mynumaricvariable2<-1:100
+mynumaricvariable2
+
+##char vector
+mycharvariable<-c("suman","Kumar","male")
+mycharvariable
+print(sum(mynumaricvariable))
