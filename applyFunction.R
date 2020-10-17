eaf6c86132b23ccddc1d9d937c470016e47023ad
Tiwari <kapil.tiwari@teradata.com>
2020-10-17 12:26:34 +0530

    added

--- applyFunction.R
+++ applyFunction.R
@@ -8,7 +8,17 @@ apply(mymatrix,1,sum)
 ##lapply-- list input list output
 mylist<-list(1:5,20:22,1:100)
 mylist
-l<-lapply(mylist,sd)
+l<-lapply(mylist,sd) #retunr list
 class(l)
 
 ##sapply 
+s<- sapply(mylist,sd) # returns vector
+class(s)
+
+##tapply
+myvar11 <- c(10,12,18,32)
+myage11 <- c('M','F','F','F')
+tapply(myvar11,myage11,mean)
+
+mtcars
+tapply(mtcars$hp,mtcars$gear,median)
