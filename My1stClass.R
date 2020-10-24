ca4f86e74b85a70195a32c9be3e4d2d87e43be20
Anurag Ranjan <anuragranjan330@gmail.com>
2020-10-24 13:51:30 +0530

    1st class

---
+++ My1stClass.R
@@ -0,0 +1,41 @@
+#1st class 
+2+3
+6*7
+#Logical values
+2+2==4
+# happy learning 
+##happy learning
+"happy learning"
+x<-42
+x
+x/7
+y = 100
+y
+y/5
+x="happy learning"
+x
+x/7
+sum(1,2 ,4)
+sqrt(5)
+rep(567,times=3)
+rep(567,5)
+rep("M Delighted", 4)
+
+#vector
+q<-c('a','b','c')
+wc<-c('Suman','raman','kundan')
+wc
+g<-5:9
+g
+h<-7:10
+h
+i<-seq(5,9)
+i
+#wc1<-("suman","abcd")
+
+j<-seq(5,9,0.5)
+j
+k=seq(9,5,-0.5)
+k
+k1=seq(9,10,0.5)
+k1
