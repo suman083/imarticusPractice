72aa44bedaad7550d6b88b2a1c32432e6df5a807
Anurag Ranjan <anuragranjan330@gmail.com>
2020-10-24 16:45:35 +0530

    ppt1st class

--- My1stClass.R
+++ My1stClass.R
@@ -39,3 +39,67 @@ k=seq(9,5,-0.5)
 k
 k1=seq(9,10,0.5)
 k1
+learn<-c('you','me','R')
+LEARN
+learn
+learn[1]
+learn[3]
+learn[5]
+learn[1:3]
+#reaplacing vector
+learn[3]<-'sas'
+learn
+learn[4]<-'spas'
+learn[5:10]<-learn ##How to replace mutliple element of vector in one go
+a<-c(1,2,3)
+b<-c(4,5,6)
+a+b
+a-b
+a*b
+#compare element
+a==c(1,4,3)
+m<-c(1,2,3,4,NA,7,9)
+sum(m)
+sum(m,na.rm = TRUE)
+help(sum)
+#matrix 
+matrix(2,3,4)
+sijio<-1:8
+s<-matrix(sijio,c(2,4))
+#matrix Access
+s[2,]
+s[1:2,]
+s[,4]
+s[,2:4]
+
+#datafreame
+#create variables and Vector
+names<-c('Joel','chris','julie','mary','sprina')
+names
+percent<-c(85,88,92,95,89) #not correct in doc
+percent
+lunch<-c('biryani','chicken kabab','biryani','chicken kabab','veg pulao')
+lunch
+str(lunch)
+yummy<-as.factor(lunch)
+yummy
+str(yummy)#factor page 33
+#create data frame
+joy<-data.frame(names,percent,yummy)
+joy
+joy$names
+joy&percent
+joy$yummy
+joy$percent
+joy[[3]]
+joy[1]
+joy[[1]]
+joy[[2]]
+joy[2]
+##Access data frame  need to ask
+joy[["yummy"]]
+
+steve<-merge(x=infantry,y=targets)
+steve
+View(steve)
+write.csv(steve,"G:/Suman/batch34/steve.csv")
