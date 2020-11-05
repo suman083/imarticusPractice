a5a6ca44b2960646be8b8cd39ac326cadf46a330
Anurag Ranjan <anuragranjan330@gmail.com>
2020-11-05 07:55:43 +0530

    Manupulation

--- DataManupulation.R
+++ DataManupulation.R
@@ -12,3 +12,22 @@ head(cs2m)
 cs2m_1<-cs2m
 ##Create new variable by another method
 cs2m_1$chlst_bp<-cs2m_1$chlstrl/cs2m_1$BP
+View(mtcars)
+View(cs2m)
+##reshape
+m=cs2m
+j=m
+dim(j)
+install.packages("reshape")
+library(reshape)
+variable.names(j)
+j=rename(j,c(DrugR='Reaction', Prgnt='Pregnent'))
+variable.names(j)
+names(j)
+
+##using
+names(j)[5]="Anxiety"
+names(j)[2]="Colesterol"
+names(j)
+cs2m_asce<-arrange(cs2m,age)
+
