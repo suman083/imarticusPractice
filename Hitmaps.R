9487530c698846108b04b9f516ff19086673f24b
Anurag Ranjan <anuragranjan330@gmail.com>
2020-11-17 16:41:47 +0530

    hitmap

--- Hitmaps.R
+++ Hitmaps.R
@@ -1,9 +1,12 @@
 
+##
 cs2m <- read.csv("G:/Suman/batch34/cs2m.csv", stringsAsFactors=TRUE)
 
 cs2m<-as.matrix(cs2m)
 heatmap(cs2m,scale = 'none')
 View(cs2m)
+
+##
 
 cd_hitmap <- read.csv("G:/Suman/batch34/project____circulatepls/cereals_data.csv", stringsAsFactors=TRUE)
 cd_hitmap<-as.matrix(cd_hitmap)
@@ -11,6 +14,8 @@ cd_hitmap<-cd_hitmap[,4:10]
 cd_hitmap
 heatmap(cd_hitmap)
 str(cd_hitmap)
+
+##
 cd_hitmap1<-apply(cd_hitmap, margin, ...)
 cd_hitmap1
 heatmap(cd_hitmap1)
