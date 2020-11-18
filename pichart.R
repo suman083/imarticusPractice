b40cd7d6e1a5126dffd62ab47ccd609edeb350e6
Anurag Ranjan <anuragranjan330@gmail.com>
2020-11-18 11:12:36 +0530

    pichart

--- pichart.R
+++ pichart.R
@@ -1,14 +1,15 @@
 
 data1<- read.csv("G:/Suman/batch34/project____circulatepls/cereals_data1.csv", stringsAsFactors=TRUE)
 view(data1)
+install.packages("RColorBrewer")
+library(RColorBrewer)
 
 
-##df %>% 
-  #filter(!is.na(data1$calories)) %>% 
-  #group_by(data1$mfr) %>% 
-  #count()
+
 
 data1$mfrFlag <- as.numeric(!is.na(data1$calories))
 pichartdata<-tapply(data1$mfrFlag , data1$mfr,sum)
 pichartdata
-as.matrix(pichartdata)
+pichartdata<-as.matrix(pichartdata)
+pie(pichartdata[,1],main = " Manufacturer of cereal",col = rainbow(length(pichartdata)))
+
