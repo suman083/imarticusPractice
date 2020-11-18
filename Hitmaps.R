17a42cf4591a5a73522f285586c3c475569c7fba
Anurag Ranjan <anuragranjan330@gmail.com>
2020-11-18 08:17:38 +0530

    hitmap

--- Hitmaps.R
+++ Hitmaps.R
@@ -1,22 +1,16 @@
 
-##
-cs2m <- read.csv("G:/Suman/batch34/cs2m.csv", stringsAsFactors=TRUE)
-
-cs2m<-as.matrix(cs2m)
-heatmap(cs2m,scale = 'none')
-View(cs2m)
-
-##
 
 cd_hitmap <- read.csv("G:/Suman/batch34/project____circulatepls/cereals_data.csv", stringsAsFactors=TRUE)
-cd_hitmap<-as.matrix(cd_hitmap)
+cd_hitmap$sodium<-cd_hitmap$sodium/100
+cd_hitmap<-data.matrix(cd_hitmap)
+View(cd_hitmap)
+
+
 cd_hitmap<-cd_hitmap[,4:10]
+#cd_hitmap<-as.numeric(cd_hitmap)
 cd_hitmap
 heatmap(cd_hitmap)
-str(cd_hitmap)
 
-##
-cd_hitmap1<-apply(cd_hitmap, margin, ...)
-cd_hitmap1
-heatmap(cd_hitmap1)
+
+
 
